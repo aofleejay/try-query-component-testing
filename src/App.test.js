0da425dd150d34d1c43e8eb3a3acaba3c68c1ece
aofleejay <aofleejay@hotmail.com>
2018-06-22 00:24:39 +0700

    Test query component when loading

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import gql from 'graphql-tag'
+import { MockedProvider } from 'react-apollo/test-utils';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Render app component when loading', () => {
+  const mocks = [
+    {
+      request: {
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+        variables: {
+          name: 'Buck',
+        },
+      },
+      result: {
+        data: {
+          dog: { id: '1', name: 'Buck', breed: 'bulldog' },
+        },
+      },
+    },
+  ];
+
+  const component = renderer.create(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <App />
+    </MockedProvider>,
+  );
+
+  const tree = component.toJSON();
+  expect(tree.children).toContain('Loading...');
 });
